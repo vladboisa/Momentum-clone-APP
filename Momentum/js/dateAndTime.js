f871c41662d779d7d5275b738dd7f0caa4997a72
VladBoisa <vladboisa@gmail.com>
2022-08-01 19:44:26 +0200

    feat: add date&time initialization

--- Momentum/js/dateAndTime.js
+++ Momentum/js/dateAndTime.js
@@ -1,1 +1,23 @@
-console.log(1);
+const timeElem = document.querySelector(".time");
+function showTime() {
+  const timeObj = new Date();
+  const currentTime = timeObj.toLocaleTimeString();
+  timeElem.textContent = currentTime;
+  // Рекурсивное обновление времени setTimeOut'ом
+  setTimeout(showTime, 1000);
+}
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+  timeZone: "GMT",
+};
+const dateElem = document.querySelector(".date");
+function showDate() {
+  const dateObj = new Date();
+  const currentDate = dateObj.toLocaleDateString("ru-RU", options);
+  dateElem.textContent = currentDate;
+}
+showTime();
+showDate();
--- Momentum/js/script.js
+++ Momentum/js/script.js
@@ -1,1 +1,4 @@
 import * as dateAndTime from "./dateAndTime.js";
+import * as greeting from "./greeting.js";
+import * as wheather from "./wheatherApi.js";
+import * as quote from "./quote.js";
