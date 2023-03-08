26fce1f6b49bcfd5b30ab94a89874e08898af43a
VladBoisa <vladboisa@gmail.com>
2023-03-08 18:38:14 +0200

    fix: styling played song elem

--- Momentum/js/audio.js
+++ Momentum/js/audio.js
@@ -14,15 +14,29 @@ const progressBarCurrentTimeElem = document.querySelector(
   ".player-currenttime"
 );
 const progressBarDurationElem = document.querySelector(".player-duration");
-
+//!TODO Styling elem icon when played next song
+//Append song's from playList
 function appendPlayList() {
-  playList.forEach((el) => {
+  playList.forEach((el, index) => {
     const li = document.createElement("li");
     li.classList.add("play-item");
     li.textContent = el.title;
+    li.dataset.playListNum = index;
+    li.dataset.playListUnicodeContent = `\u2715`; //styling data attribute of ::before elem with unicode Icon of mistake
     playListElem.appendChild(li);
   });
 }
+//Play song by click on list of item's
+function playSongByClick(event) {
+  audio.src = playList[event.target.dataset.playListNum].src;
+  audio.play();
+  event.target.dataset.playListUnicodeContent = `\u2713`; //styling data attribute of ::before elem with unicode Icon of correct
+  event.target.style.setProperty('--color-playlist-item',"#2EFF2E")
+  playElem.classList.add("pause");
+  isPlay = true;
+}
+playListElem.addEventListener("click", playSongByClick);
+
 function pauseAudio() {
   isPlay = false;
   audio.pause();
@@ -72,8 +86,8 @@ playPrevElem.addEventListener("click", playPrev);
 playElem.addEventListener("click", playAudio);
 // automatically play the next song at the end of the audio object's duration
 audio.addEventListener("ended", playNext);
-// function where progressBar.value is changed when slider thumb is dragged
-progressBarElem.addEventListener("input", (e) => {
+// when slider thumb is dragged - progressBar.value will be changed-
+progressBarElem.addEventListener("input", () => {
   audio.currentTime = progressBarElem.value;
 });
 appendPlayList();
