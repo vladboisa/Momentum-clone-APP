13f9f8b02bd5d529b5eca3540071da0f6da5d706
VladBoisa <vladboisa@gmail.com>
2023-04-28 19:49:07 +0200

    refactor: prettier formatting

--- Momentum/src/app.ts
+++ Momentum/src/app.ts
@@ -1,9 +1,7 @@
-import './css/style.css'
-import './ts/render' //First module to import, cause rendering
-import './ts/backgroundImg';
+import "./css/style.css";
+import "./ts/render"; //First module to import, cause rendering
+import "./ts/backgroundImg";
 import "./ts/weatherApi";
 import "./ts/quote";
 import "./ts/audio";
 import "./ts/dateAndTime";
-
-
